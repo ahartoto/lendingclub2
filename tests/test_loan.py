96ac96fa10258b44fa9957074368990798471ec8
Alex Hartoto <ahartoto.dev@gmail.com>
2017-07-06 23:37:43 -0700

    Skip test for searching loan if cannot find auth key

--- tests/test_loan.py
+++ tests/test_loan.py
@@ -7,12 +7,22 @@ Test the lendingclub2.loan module
 # Standard libraries
 import collections
 
+# PyTest
+import pytest
+
 # lendingclub2
 from lendingclub2 import loan
+from lendingclub2.authorization import Authorization
+from lendingclub2.error import LCError
 
 
 class TestListing(object):
     def test_search(self):
+        try:
+            _ = Authorization.key
+        except LCError:
+            pytest.skip("skip test: cannot find authorization key")
+
         listing = loan.Listing()
         listing.search()
         assert isinstance(listing.loans, collections.Iterable)
