3187e8c3407d5bfc29f89966f4147e264e71eeb0
Alex Hartoto <ahartoto.dev@gmail.com>
2017-07-12 22:46:10 -0700

    Release 0.1.0

--- lendingclub2/__init__.py
+++ lendingclub2/__init__.py
@@ -1,5 +1,8 @@
 """
-LendingClub API
+Unofficial Python Lending Club API
+
+Using this API user should be able to find necessary information of their
+investor account as well as placing orders.
 """
 
-__version__ = "0.0.1"
+__version__ = "0.1.0"
